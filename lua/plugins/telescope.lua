2bc83e8fa32d2b66038e39f8ebb17e70ccc77c95
Daniel Perez <doperez1997@gmail.com>
2025-10-26 20:17:05 -0600

    Added more telescope bindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,8 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	config = function()
+    vim.keymap.set("n", "<space>lg", require("telescope.builtin").live_grep)
+    vim.keymap.set("n", "<space>bi", require("telescope.builtin").builtin)
 		vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
 		vim.keymap.set("n", "<space>en", function()
 			require("telescope.builtin").find_files({
