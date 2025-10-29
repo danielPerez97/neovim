003afcb15adea82775cccb168fddbe60c1df7840
danielPerez97 <doperez1997@gmail.com>
2025-10-28 20:45:15 -0600

    lsp improvements

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,20 +3,20 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-    "saghen/blink.cmp",
+		"saghen/blink.cmp",
 	},
 	config = function()
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
-        "clangd",
+				"clangd",
 			},
 		})
 
-		local capabilities = require('blink.cmp').get_lsp_capabilities()
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		vim.lsp.config('*', {
+		vim.lsp.config("*", {
 			capabilities = capabilities,
 		})
 
@@ -27,8 +27,24 @@ return {
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
 			end,
 		})
+
+		vim.api.nvim_create_autocmd("CursorHold", {
+			callback = function()
+				local opts = {
+					focusable = false,
+					close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+					border = "rounded",
+					source = "always",
+					prefix = " ",
+				}
+				vim.diagnostic.open_float(nil, opts)
+			end,
+		})
+
+		vim.opt.updatetime = 250
 
 		vim.lsp.config.lua_ls = {
 			cmd = { "lua-language-server" },
