c36b44b83465c5ddc5454882579ee67acdc0a690
danielPerez97 <doperez1997@gmail.com>
2025-11-16 11:11:53 -0700

    reformat blink.lua

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,25 +1,25 @@
 return {
-  'saghen/blink.cmp',
-  enable = false,
-  dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	enable = false,
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-  version = '1.*',
+	version = "1.*",
 
-  opts = {
-    keymap = { preset = 'default' },
+	opts = {
+		keymap = { preset = "super-tab" },
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = { documentation = { auto_show = false } },
+		completion = { documentation = { auto_show = false } },
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
 
-  opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
