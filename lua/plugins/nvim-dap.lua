2f3a3d575652f92e054f3e78603e967b113f4561
Daniel Perez <doperez1997@gmail.com>
2026-05-05 22:01:24 -0500

    tune nvim-dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,18 +1,49 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "wojciech-kulik/xcodebuild.nvim"
-  },
-  config = function()
-    local xcodebuild = require("xcodebuild.integrations.dap")
-    xcodebuild.setup()
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"wojciech-kulik/xcodebuild.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local xcodebuild = require("xcodebuild.integrations.dap")
 
-    vim.keymap.set("n", "<leader>dd", xcodebuild.build_and_debug, { desc = "Build & Debug" })
-    vim.keymap.set("n", "<leader>dr", xcodebuild.debug_without_build, { desc = "Debug Without Building" })
-    vim.keymap.set("n", "<leader>dt", xcodebuild.debug_tests, { desc = "Debug Tests" })
-    vim.keymap.set("n", "<leader>dT", xcodebuild.debug_class_tests, { desc = "Debug Class Tests" })
-    vim.keymap.set("n", "<leader>b", xcodebuild.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-    vim.keymap.set("n", "<leader>B", xcodebuild.toggle_message_breakpoint, { desc = "Toggle Message Breakpoint" })
-    vim.keymap.set("n", "<leader>dx", xcodebuild.terminate_session, { desc = "Terminate Debugger" })
-  end,
+		-- Adapters
+		dap.adapters.lldb = {
+			type = "executable",
+			command = vim.fn.exepath("lldb-dap"),
+			name = "lldb",
+		}
+
+		-- Configurations
+		dap.configurations.rust = {
+			{
+				name = "Launch binary",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					local metadata = vim.fn.json_decode(vim.fn.system("cargo metadata --no-deps --format-version 1"))
+					local name = metadata.packages[1].name:gsub("-", "_")
+					local result = vim.fn.system("cargo build")
+					if vim.v.shell_error ~= 0 then
+						print("cargo build failed:\n" .. result)
+						return nil
+					end
+					return vim.fn.getcwd() .. "/target/debug/" .. name
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
+		-- Keymaps
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue / Launch" })
+		vim.keymap.set("n", "<leader>dx", dap.terminate, { desc = "Terminate Debugger" })
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
+		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+		vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Step Out" })
+		vim.keymap.set("n", "<leader>B", function()
+			dap.set_breakpoint(nil, nil, vim.fn.input("Log message: "))
+		end, { desc = "Toggle Message Breakpoint" })
+	end,
 }
